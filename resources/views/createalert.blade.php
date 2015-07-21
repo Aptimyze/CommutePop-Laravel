531f14bb3e002925cadec40567466689161b41b6
Greg Kaleka <greg@gregkaleka.com>
2015-07-20 23:51:04 -0700

    Changed name of alert creation file

--- resources/views/createalert.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.main')
-@section('content')
-    <h1>Create an alert</h1>
-    <p>Great! Let's get you set up. Fill out the form below, and we'll see what routes and stops you might want alerts for.</p>
-    
-@stop
