9df99d8b6f851e49b17d05337292d9fda5b2032a
Greg Kaleka <greg.kaleka@gmail.com>
2015-08-01 11:41:54 -0700

    Created alert email view

---
+++ resources/views/emails/alertemail.blade.php
@@ -0,0 +1,4 @@
+<h2>Your commute options</h2>
+@yield('content')
+<p>Thanks for using CommutePop! As always, you can let us know what you think by responding to this email.</p>
+<p>To cancel this alert, <a href="commutepop.com/cancel/?alertid={{{ $alert->id }}}">click here.</a></p>
