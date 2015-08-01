74c36e1ee3b0801e3e3f950d7b841bda6b622cfb
Greg Kaleka <greg.kaleka@gmail.com>
2015-08-01 13:56:01 -0700

    Commenting out some code for testing

--- resources/views/emails/alertemail.blade.php
+++ resources/views/emails/alertemail.blade.php
@@ -1,7 +1,7 @@
 <h2>Hi! We got the info below from Trimet at {{{ $emailData['queryTime'] }}}.</h2>
 <h2>When your ride leaves from {{{ $emailData['stopName'] . $emailData['routeDirection'] }}}:</h2>
 <ul>
-@foreach ($emailData['arrivalTimes'] as $arrivalTime)
+{{-- @foreach ($emailData['arrivalTimes'] as $arrivalTime)
 	<li>{{{ $arrivalTime }}}</li>
 @endforeach
 </ul>
@@ -10,6 +10,6 @@
 @foreach ($emailData['deskDepartures'] as $deskDeparture)
 	<li>{{{ $deskDeparture }}}</li>
 @endforeach
-</ul>
+</ul> --}}
 <p>Thanks for using CommutePop! As always, you can let us know what you think by responding to this email.</p>
 {{-- <p>To cancel this alert, <a href="commutepop.com/cancel/?alertid={{{ $alert->id }}}">click here.</a></p> --}}
