a8cefae87728f77e7fb4aef92c1b05baa789d11c
Greg Kaleka <greg@gregkaleka.com>
2015-08-02 03:05:38 +0000

    Making some tweaks to alert email

--- resources/views/emails/alertemail.blade.php
+++ resources/views/emails/alertemail.blade.php
@@ -1,3 +1,10 @@
+<!DOCTYPE xhtml1-strict>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<style type="text/css">
+	/* ///// RESET STYLES /////*/
+	</style>
+</head>
 <h3>Hi! Here's your CommutePop Alert.</h3>
 <h4>FYI, we got the info below from Trimet at {{ $emailData['queryTime'] }}.</h4>
 <h5>When your upcoming rides leave from {{ $emailData['stopName'] . $emailData['routeDirection'] }}:</h5>
@@ -13,4 +20,4 @@
 @endforeach
 </ol>
 <p>Thanks for using CommutePop! As always, you can let us know what you think by responding to this email.</p>
-{{-- <p>To cancel this alert, <a href="commutepop.com/cancel/?alertid={{{ $alert->id }}}">click here.</a></p> --}}
+{{-- <p>To edit or cancel this alert, <a href="commutepop.com/cancel/{!! $alert->id !!}">click here.</a></p> --}}
