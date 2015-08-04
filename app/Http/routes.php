a64a81d01ce9668ab1742684178f57832937dac9
Greg Kaleka <greg@gregkaleka.com>
2015-08-04 00:26:58 -0700

    Cleaned up the routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,11 +11,16 @@
 |
 */
 
-	Route::get('/alerts', function () { return view('alerts'); });
-	Route::get('/alerts/new', ['as' => 'alerts.new', 'uses' => 'AlertCreationController@create']);
-	Route::post('/alerts/new/confirm', ['as' => 'alerts.confirm', 'uses' => 'AlertCreationController@store']);
-	Route::get('admin', 'AlertCreationController@index');
+Route::get('/alert', function () { return view('index'); });
+Route::get('/alerts/new', ['as' => 'alerts.new', 'uses' => 'AlertCreationController@create']);
+Route::post('/alerts/new/confirm', ['as' => 'alerts.confirm', 'uses' => 'AlertCreationController@store']);
+Route::get('admin', 'AlertCreationController@index');
 
 Route::get('/', ['as' => 'landing.optin', 'uses' => 'LandingController@create']);
-
 Route::post('/', ['as' => 'landing.confirm', 'uses' => 'LandingController@store']);
+
+Route::get('/sendthemall', function () { 
+	$handler = new AlertHandler();
+    $handler->sendAlertEmails(5);
+    return '<a href="http://commutepop.com/">CommutePop</a>;
+});
