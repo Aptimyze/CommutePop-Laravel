26ded0f609555d722bf41b341ba10246bfaaed75
Greg Kaleka <greg@gregkaleka.com>
2015-08-16 04:02:41 +0000

    Adds tests for successful and failed login attempts

--- features/authentication.feature
+++ features/authentication.feature
@@ -8,7 +8,14 @@ Feature: Membership
     When I register "JonDoe" "john@example.com"
     Then I should have an account
 
+  Scenario: Successful Authentication
+    Given I have an account "JohnDoe" "john@example.com"
+    When I sign in
+    Then I should be logged in
 
+  Scenario: Failed Authentication
+    When I sign in with invalid credentials
+    Then I should not be signed in
 
 
     # Given I am on "/alerts"
--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -6,27 +6,27 @@ use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\MinkContext;
 use PHPUnit_Framework_Assert as PHPUnit;
+use Laracasts\Behat\Context\Migrator;
+use Laracasts\Behat\Context\DatabaseTransactions;
+
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
 {
-    /**
-     * @Then I should be able to do something with Laravel
-     */
-    public function iShouldBeAbleToDoSomethingWithLaravel()
-    {
-        $environmentFileName = app()->environmentFile();
-        $environmentName = env('APP_ENV');
-        PHPUnit::assertEquals('.env.behat', $environmentFileName);
-        PHPUnit::assertEquals('acceptance', $environmentName);
-    }
+    use Migrator, DatabaseTransactions;
+
+    private $name;
+    private $email;
 
     /**
      * @When I register :name :email
      */
     public function iRegister($name, $email)
     {
+        $this->name = $name;
+        $this->email = $email;
+
         $this->visit('auth/register');
 
         $this->fillField('name', $name);
@@ -35,7 +35,6 @@ class FeatureContext extends MinkContext implements Context, SnippetAcceptingCon
         $this->fillField('password_confirmation', 'password');
 
         $this->pressButton('register');
-        $this->printLastResponse();
     }
 
     /**
@@ -43,6 +42,61 @@ class FeatureContext extends MinkContext implements Context, SnippetAcceptingCon
      */
     public function iShouldHaveAnAccount()
     {
-        throw new PendingException();
+        $this->assertSignedIn();
+    }
+
+    /**
+     * @Given I have an account :name :email
+     */
+    public function iHaveAnAccount($name, $email)
+    {
+        $this->iRegister($name, $email);
+        $this->visit('auth/logout');
+    }
+
+    /**
+     * @When I sign in
+     */
+    public function iSignIn()
+    {
+        $this->visit('auth/login');
+
+        $this->fillField('email', $this->email);
+        $this->fillField('password', 'password');
+        $this->pressButton('login-button');
+    }
+
+    /**
+     * @When I sign in with invalid credentials
+     */
+    public function iSignInWithInvalidCredentials()
+    {
+        $this->email = 'invalid@example.com';
+        $this->iSignIn();
+    }
+
+    /**
+     * @Then I should be logged in
+     */
+    public function iShouldBeLoggedIn()
+    {
+        $this->assertSignedIn();
+    }
+
+    /**
+     * @Then I should not be signed in
+     */
+    public function iShouldNotBeSignedIn()
+    {
+        PHPUnit::assertTrue(Auth::guest());
+
+        $this->assertPageAddress('auth/login');
+        $this->assertPageContainsText('These credentials do not match our records.');
+    }
+
+    private function assertSignedIn()
+    {
+        PHPUnit::assertTrue(Auth::check());
+        $this->assertPageAddress('/alerts');
     }
 }
